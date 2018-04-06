6ad3c1158909d2e20c0c65e57af6ca6f626d9380
Pat Shiu <shiu.patricia@gmail.com>
2018-04-06 12:34:07 -0400

    schiwing

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 var arrayOfDots = [];
 function setup() {
 createCanvas(400, 3000);
-background(0);
+background(255);
 }
 
 function draw() {
- background(0);
+ background(255);
  text(frameRate(), 10,10);
  fadeDots();
 }
@@ -27,9 +27,9 @@ function fadeDots() {
  for(i = arrayOfDots.length-1; i > 0; i--){
    if (arrayOfDots[i].opacity > 0){
      arrayOfDots[i].opacity = arrayOfDots[i].opacity - 25;
-     fill(255, arrayOfDots[i].opacity);
+     fill(0, arrayOfDots[i].opacity);
      noStroke();
-     ellipse(arrayOfDots[i].oX, arrayOfDots[i].oY, 60, 60);
+     ellipse(arrayOfDots[i].oX, arrayOfDots[i].oY, 120, 120);
    } else {
      arrayOfDots.splice(i,1);
      print("one dead, total left = " + arrayOfDots.length);
